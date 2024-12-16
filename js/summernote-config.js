17d1a3b0e9adba2dc62c3ae65d7a2531af199815
Rafael Fiuza <61255847+V1PERFX@users.noreply.github.com>
2024-12-16 19:17:48 -0300

    Initial commit
    Adicionando os arquivos ao GitHub

---
+++ js/summernote-config.js
@@ -0,0 +1,15 @@
+$('#summernote').summernote({
+  placeholder: 'Digite aqui seu texto...',
+  tabsize: 2,
+  height: 400,
+  lang: 'pt-BR',
+  fontNames: ['Poppins', 'Designer', 'Arial', 'Arial Black', 'Courier New', 'Helvetica Neue', 'Helvetica', 'Chakra Petch', 'Tahoma', 'Times New Roman', 'Verdana'],
+  fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '24', '36'],
+  toolbar: [
+    // [groupName, [list of button]]
+    ['Font Style', ['fontname', 'fontsize', 'fontsizeunit', 'color', 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+    ['Paragraph style', ['style', 'ol', 'ul', 'paragraph', 'height']],
+    ['Insert', ['picture', 'link', 'video', 'table', 'hr']],
+    ['Misc', ['fullscreen', 'codeview', 'undo', 'redo', 'help']]
+  ]
+});
